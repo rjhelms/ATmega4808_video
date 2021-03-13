a39790f7b9db8f2f6d5059bd07d80c7d908462f2
Rob Hailman <robhailman@gmail.com>
2021-03-13 03:01:59 -0500

    Initial commit

---
+++ src/colors.h
@@ -0,0 +1,21 @@
+/*
+  colors.h
+  Color definitions for the AD723duino
+*/
+
+#define BLACK           0x0
+#define DARK_RED        0x1
+#define DARK_GREEN      0x2
+#define DARK_YELLOW     0x3
+#define DARK_BLUE       0x4
+#define DARK_MAGENTA    0x5
+#define DARK_CYAN       0x6
+#define LIGHT_GRAY      0x7
+#define DARK_GRAY       0x8
+#define RED             0x9
+#define GREEN           0xA
+#define YELLOW          0xB
+#define BLUE            0xC
+#define MAGENTA         0xD
+#define CYAN            0xE
+#define WHITE           0xF
