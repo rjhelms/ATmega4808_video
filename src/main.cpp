6c1b22baa5b65579d556e487f37578f5581d191e
Rob Hailman <robhailman@gmail.com>
2021-03-14 15:54:41 -0400

    Use a shift register to shift out nibbles
    This is the wrong approach - too many writes!

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@
 #include <util/delay_basic.h>
 #include "colors.h"
 #include "wait_until.h"
-#include "img/ducky.h"
+#include "img/ducky16.h"
 
 // times defined in cycles - determine programmatically???
 const uint16_t LINE_PERIOD = 1271;
 const uint16_t HSYNC_PULSE = 94;
 const uint16_t FRONT_PORCH = 30;
 const uint16_t BACK_PORCH = 91;
-const uint16_t BORDER_WIDTH = 80;
-const uint16_t FRONT_PORCH_FUDGE = FRONT_PORCH + 12; // fudged front porch, to make sure we get into the interrupt in time
+const uint16_t BORDER_WIDTH = 0;
+const uint16_t FRONT_PORCH_FUDGE = FRONT_PORCH + 24; // fudged front porch, to make sure we get into the interrupt in time
 const uint16_t FIELD_LINES = 262;
 
 const uint16_t BORDER_START = 23;
@@ -22,21 +22,53 @@ const uint16_t PICTURE_START = 45;
 const uint16_t PICTURE_END = 237;
 const uint16_t BORDER_END = 259;
 
-const uint8_t LINES_PER_PIXEL = 3;
+const uint8_t LINES_PER_PIXEL = 12;
 
-const uint8_t X = 32;
-const uint8_t Y = 64;
+const uint8_t X = 16;
+const uint8_t Y = 16;
+
+const uint8_t SDATA = PIN0_bm;
+const uint8_t SCLK = PIN2_bm;
+const uint8_t SLATCH = PIN1_bm;
+const uint8_t PIXEL_SHIFT_MASK = 0x1;
 
 uint16_t palette = (YELLOW << 12 | GREEN << 8 | RED << 4 | BLACK << 0);
 uint16_t field_line = 0;
 uint16_t pixel_line = 0;
-uint16_t frame = 0;
 
-uint8_t *screen = (uint8_t *)malloc(X * Y);
-uint8_t *screen_line = screen;
-uint8_t *screen_pixel = screen;
+volatile uint16_t frame = 0;
+volatile uint8_t *screen = (uint8_t *)malloc(X * Y);
+volatile uint8_t *screen_line = screen;
+volatile uint8_t *screen_pixel = screen;
 
 uint16_t val = 0;
+
+inline void shift_nibble(uint8_t val)
+{
+  VPORTC_OUT = val & PIXEL_SHIFT_MASK;
+  VPORTC_OUT |= SCLK;
+  val >>= 1;
+  VPORTC_OUT = (val & PIXEL_SHIFT_MASK);
+  VPORTC_OUT |= SCLK;
+  val >>= 1;
+  VPORTC_OUT = (val & PIXEL_SHIFT_MASK);
+  VPORTC_OUT |= SCLK;
+  val >>= 1;
+  VPORTC_OUT = (val & PIXEL_SHIFT_MASK);
+  VPORTC_OUT |= SCLK;
+  VPORTC_OUT |= SLATCH;
+}
+
+inline void shift_pixel(uint8_t *val)
+{
+  VPORTC_OUT = *val & PIXEL_SHIFT_MASK;
+  VPORTC_OUT |= SCLK;
+  *val >>= 1;
+  VPORTC_OUT = (*val & PIXEL_SHIFT_MASK);
+  VPORTC_OUT |= SCLK;
+  VPORTC_OUT |= SLATCH;
+  *val >>= 1;
+}
 
 int main()
 {
@@ -64,13 +96,36 @@ int main()
 
   sei();
 
-  for (int i = 0; i < X * Y; i++)
+  for (int i = 0; i < (X * Y / 2); i++)
   {
-    screen[i] = pgm_read_byte(ducky + i);
+    screen[i] = i;
   }
 
+  bool ducky = false;
+  volatile uint16_t local_frame = 0;
   while (true)
   {
+    if (frame - local_frame >= 0xA0)
+    {
+      if (ducky)
+      {
+        for (int i = 0; i < X * Y; i++)
+        {
+          screen[i] = i;
+          // screen[i] = pgm_read_byte(ducky16 + i);
+        }
+        ducky = false;
+      }
+      else
+      {
+        for (int i = 0; i < X * Y; i++)
+        {
+          screen[i] = pgm_read_byte(ducky16 + i);
+        }
+        ducky = true;
+      }
+      local_frame += 0xA0;
+    }
   }
 
   return 0;
@@ -79,7 +134,7 @@ int main()
 ISR(TCA0_CMP0_vect) // TCA0 CPM0 routine - front porch
 {
   VPORTA_OUT |= PIN3_bm; // BLANK high
-  VPORTC_OUT = 0x00;     // blanking interval - output black
+  //shift_nibble(BLACK);   // left frame border - output color 0
 
   switch (field_line) // line-specific thingies
   {
@@ -87,6 +142,8 @@ ISR(TCA0_CMP0_vect) // TCA0 CPM0 routine - front porch
     // line 0 - set vsync high
     PORTA.OUT |= PIN1_bm;
     frame++;
+    screen[127] = frame;
+    screen[126] = frame >> 8;
     break;
   case 4:
     // line 1 - set vsync low
@@ -94,11 +151,11 @@ ISR(TCA0_CMP0_vect) // TCA0 CPM0 routine - front porch
     break;
   case BORDER_START:
     // border start - enable overflow interrupt
-    TCA0.SINGLE.INTCTRL |= TCA_SINGLE_OVF_bm;
+    // TCA0.SINGLE.INTCTRL |= TCA_SINGLE_OVF_bm;
     break;
   case BORDER_END:
     // border end - disable overflow interrupt
-    TCA0.SINGLE.INTCTRL &= ~TCA_SINGLE_OVF_bm;
+    // TCA0.SINGLE.INTCTRL &= ~TCA_SINGLE_OVF_bm;
     break;
   case FIELD_LINES:
     field_line = 0;
@@ -123,19 +180,43 @@ ISR(TCA0_CMP0_vect) // TCA0 CPM0 routine - front porch
 
 ISR(TCA0_CMP1_vect) // TCA0 CPM1 routine - start of drawing period
 {
-  for (int i = 0; i < 32; i++) // loop to push pixels
+
+  uint8_t draw_byte;
+
+  for (int i = 0; i < 8; i++) // loop to push pixels
   {
-    VPORTC_OUT = *screen_pixel;
-    screen_pixel++;
+
+    draw_byte = *screen_pixel++;
+    shift_nibble(draw_byte);
+    shift_nibble(draw_byte >> 4);
+    // VPORTC_OUT = draw_byte & PIXEL_SHIFT_MASK;
+    // VPORTC_OUT |= SCLK;
+    // draw_byte >>= 1;
+
+    // latch high
+
+    // wait 'til next pixel
+
+    // clock low, bit 5
+    // clock high, latch low
+    // clock low, bit 6
+    // clock high
+    // clock low, bit 7
+    // clock high
+    // clock low, bit 8
+    // clock high
+    // latch high
+
+    // wait 'til next pixel
   }
   _delay_loop_1(1);
   TCA0.SINGLE.INTFLAGS = TCA_SINGLE_CMP1_bm; // clear CMP1 interrupt flag
-  VPORTC_OUT = palette;                      // right border - output color 0
+  shift_nibble(0);                           // left frame border - output color 0
   pixel_line++;
   if (pixel_line == LINES_PER_PIXEL)
   {
     pixel_line = 0;
-    screen_line += 32;
+    screen_line += 8;
   }
   screen_pixel = screen_line;
 }
@@ -147,5 +228,5 @@ ISR(TCA0_OVF_vect) // TCA0 overflow routine - each line
   wait_until(HSYNC_PULSE + BACK_PORCH); // wait until start of active period
 
   VPORTA_OUT &= ~PIN3_bm; // BLANK low
-  VPORTC_OUT = palette;   // left frame border - output color 0
+  // shift_nibble(RED);   // left frame border - output color 0
 }
